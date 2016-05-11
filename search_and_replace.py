20a4497ab39e6568e47d583b20219f2ed6dcc071
Harbingers <financesmacro@gmail.com>
2016-05-11 16:31:32 -0400

    Create search_and_replace.py

---
+++ search_and_replace.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+import os
+
+def search_and_replace(dir, old_str, new_str):
+    for dname, dirs, files in os.walk(dir):
+        for fname in files:
+            fpath = os.path.join(dname, fname)
+            with open(fpath) as f:
+                s = f.read()
+            s = s.replace(oldstr, newstr)
+            with open(fpath, "w") as f:
+                f.write(s)
+                
+search_and_replace(".", "old_str", "new_str")
