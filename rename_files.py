f7ac48bf0dc86a9d32f0d462ba66abb21821cba2
Harbingers <financesmacro@gmail.com>
2016-05-11 15:19:25 -0400

    Update rename_files.py

--- rename_files.py
+++ rename_files.py
@@ -1,1 +1,8 @@
+#!/usr/bin/env python
 
+# rename files in batch for a certain folder
+def replace(fpath, old_str, new_str):
+    for path, subdirs, files in os.walk(fpath):
+        for name in files:
+            if(old_str.lower() in name.lower()):
+                os.rename(os.path.join(path,name), os.path.join(path, name.lower().replace(old_str,new_str)))
